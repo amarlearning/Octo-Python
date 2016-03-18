e62856cf9b0e73098a38a6264c06fac18988c9d8
Amar Prakash Pandey <amarlearning@users.noreply.github.com>
2016-03-19 02:52:48 +0530

    Update draw_square.py

--- draw_square.py
+++ draw_square.py
@@ -12,28 +12,28 @@ def draw_circle(turtle_name) :
 
 def draw_triangle(turtle_name) :
     turtle_name.left(180)
-    turtle_name.forward(100)
-    turtle_name.right(240)
-    turtle_name.forward(100)
-    turtle_name.right(240)
+    i = 1
+    while(i<3) :
+        turtle_name.forward(100)
+        turtle_name.right(240)
     turtle_name.forward(100)
 
 window = turtle.Screen()
 window.bgcolor("yellow")
 
 brad = turtle.Turtle()
-brad.color("green")
-brad.shape("turtle")
-draw_square(brad)
+    brad.color("green")
+    brad.shape("turtle")
+    draw_square(brad)
 
 pit = turtle.Turtle()
-pit.color("red")
-pit.shape("circle")
-draw_circle(pit)
+    pit.color("red")
+    pit.shape("circle")
+    draw_circle(pit)
 
 trip = turtle.Turtle()
-trip.color("blue")
-trip.shape("triangle")
-draw_triangle(trip)
+    trip.color("blue")
+    trip.shape("triangle")
+    draw_triangle(trip)
 
 window.exitonclick()
